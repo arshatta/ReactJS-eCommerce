7c745e384f2ee2a1c67b9581f2ab40bd5d53ecb4
Arshat Talap <t_arshat@mail.ru>
2022-06-20 12:55:16 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
+import Categories from "./components/Categories";
+import ShowFullItem from "./components/ShowFullItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      orders: [],
+      currentItems: [],
+      items: [
+        {
+          id: 1,
+          title: 'Стул первый',
+          img: 'chair-grey.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'Стол',
+          img: 'table.webp',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'tables',
+          price: '149.00'
+        },
+        {
+          id: 3,
+          title: 'Диван',
+          img: 'sofa.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'sofa',
+          price: '549.99'
+        },
+        {
+          id: 4,
+          title: 'Лампа',
+          img: 'wall-light.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'light',
+          price: '25.00'
+        },
+        {
+          id: 5,
+          title: 'Стул белый',
+          img: 'chair-white.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'chairs',
+          price: '49.99'
+        }
+      ],
+      showFullItem: false,
+      fullItem: {}
+    }
+    this.state.currentItems = this.state.items
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
+    this.chooseCategory = this.chooseCategory.bind(this)
+    this.onShowItem = this.onShowItem.bind(this)
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <Categories chooseCategory={this.chooseCategory} />
+        <Items onShowItem={this.onShowItem} items={this.state.currentItems} onAdd={this.addToOrder}/>
+
+        {this.state.showFullItem && <ShowFullItem onAdd={this.addToOrder} onShowItem={this.onShowItem} item={this.state.fullItem} />}
+        <Footer />
+      </div>
+    )
+  }
+
+  onShowItem(item) {
+    this.setState({fullItem: item})
+    this.setState({showFullItem: !this.state.showFullItem })
+  }
+
+  chooseCategory(category) {
+    if(category === 'all') {
+      this.setState({currentItems: this.state.items})
+      return 
+    }
+
+    this.setState({
+      currentItems: this.state.items.filter(el => el.category === category)
+    })
+  }
+
+  deleteOrder(id) {
+    this.setState({orders: this.state.orders.filter(el => el.id !== id)})
+  }
+  
+  addToOrder(item) {
+    let isInArray = false
+    this.state.orders.forEach(el => {
+      if(el.id === item.id)
+        isInArray = true
+    })
+    if(!isInArray)
+      this.setState({ orders: [...this.state.orders, item] })
+  }
 }
 
-export default App;
+export default App;  
